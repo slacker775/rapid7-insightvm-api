63d7b8a19d40b282849243bef7126a63545ebcd8
David Hollis <dhollis@davehollis.com>
2021-10-21 13:05:55 -0400

    Initial commit

---
+++ src/Factory/ClientFactory.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rapid7\InsightVM\Api\Factory;
+
+use Http\Client\Common\Plugin;
+use Http\Client\Common\PluginClient;
+use Http\Discovery\Psr17FactoryDiscovery;
+use Http\Discovery\Psr18ClientDiscovery;
+use Http\Message\Formatter\FullHttpMessageFormatter;
+use Jane\OpenApiRuntime\Client\Plugin\AuthenticationRegistry;
+use Psr\Http\Client\ClientInterface;
+use Psr\Log\LoggerInterface;
+use Rapid7\InsightVM\Api\Authentication\BasicAuthentication;
+use Rapid7\InsightVM\Api\Client;
+
+class ClientFactory
+{
+
+    public static function create(string $host, string $username,
+        string $password,
+        ClientInterface $httpClient = null,
+        array $additionalPlugins = []
+    ): Client {
+        if (null === $httpClient) {
+            $httpClient = Psr18ClientDiscovery::find();
+        }
+        $plugins = [
+            new Plugin\AddHostPlugin(
+                Psr17FactoryDiscovery::findUriFactory()->createUri($host)
+            ),
+            new AuthenticationRegistry([
+                new BasicAuthentication(
+                    $username, $password
+                ),
+            ]),
+        ];
+        if (count($additionalPlugins) > 0) {
+            $plugins = array_merge($plugins, $additionalPlugins);
+        }
+        $httpClient = new PluginClient(
+            $httpClient, $plugins
+        );
+
+        return Client::create($httpClient);
+    }
+
+    public static function getDebugLogger(LoggerInterface $logger,
+        int $maxBodyLength = 1000
+    ): Plugin {
+        return new Plugin\LoggerPlugin(
+            $logger,
+            new FullHttpMessageFormatter($maxBodyLength)
+        );
+    }
+
+}
